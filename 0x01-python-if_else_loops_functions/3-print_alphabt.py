9334abacfca9aa3afbd871d148abe14d29387cc9
Julesswiss <jerhivwor@gmail.com>
2022-11-29 04:09:55 -0800

    Still doing simple python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for ch in range(97, 123):
-        if ch != 101 and ch != 113:
-                print("{:c}".format(ch), end='')
+for m in range(97, 123):
+    if(m != 101 and m != 113):
+        print("{}".format(chr(m)), end='')
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for num in range(0, 100):
-        if num != 99:
-                print("{:02d}, ".format(num), end='')
-        else:
-                print("{:02d}".format(num))
+for digit in range(0, 100):
+    if digit == 99:
+        print("{}".format(digit))
+    else:
+        print("{:02}".format(digit), end=',')
