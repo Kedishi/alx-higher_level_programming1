e318c2160e93726cbe3fd2434b586eb958ed0b24
Julesswiss <jerhivwor@gmail.com>
2022-12-01 03:46:20 -0800

    correcting again

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3 
- import sys 
+import sys 
   
- num_of_args = len(sys.argv) - 1 
-  
- if num_of_args == 0: 
+num_of_args = len(sys.argv) - 1 
+if num_of_args == 0: 
      print("{} arguments.".format(num_of_args)) 
- elif num_of_args == 1: 
+elif num_of_args == 1: 
      print("{} argument:".format(num_of_args)) 
      print("{}: {}".format(num_of_args, sys.argv[1])) 
- else: 
+else: 
      print("{} arguments:".format(num_of_args)) 
      # loop through the list and print 
      for i in range(num_of_args + 1): 
